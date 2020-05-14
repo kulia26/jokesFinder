86cdcc19d90d254b04897d8c2f1cc0777ca4f491
Bohdan Kulinich <kulinich261@gmail.com>
2020-05-14 19:44:20 +0300

    html/css/animations done

---
+++ animations.js
@@ -0,0 +1,29 @@
+const animate = () => {
+  const menuInput = document.getElementById('menu');
+  menuInput.addEventListener('change', (event)=>{
+    const wrap = document.querySelector('.wrap');
+    const aside = document.getElementsByTagName('aside')[0];
+    let button = document.querySelector('div.menu');
+    if(event.target.checked){
+      setTimeout(()=>{
+        aside.appendChild(menuInput)
+        aside.appendChild(button);
+      }, 550)
+    }else{
+      wrap.appendChild(menuInput)
+      wrap.appendChild(button);
+      menuInput.checked = true
+      console.log(menuInput.checked)
+      setTimeout(()=>{
+        menuInput.checked = false;
+      }, 20)
+    }
+    
+    aside.classList.toggle('show');
+    aside.classList.toggle('hide');
+    document.body.classList.toggle('blur-show');
+    document.body.classList.toggle('blur-hide');
+  })
+}
+
+window.onload = animate;
